40494e67a0ee9ef8bfb52485d2f97f5eeb64c248
koltsov_ml <koltsov_ml@groupbwt.com>
2021-05-10 19:45:53 +0300

    feat(api): add create service tests

--- src/Service/Task/CreateTaskService.php
+++ src/Service/Task/CreateTaskService.php
@@ -1,20 +1,40 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Service\Task;
 
 use App\Dto\Api\Task\CreateTaskDto;
 use App\Entity\Task;
 use App\Manager\DoctrineManager;
 
+/**
+ * Class CreateTaskService
+ * @package App\Service\Task
+ */
 class CreateTaskService
 {
+    /**
+     * @var DoctrineManager
+     */
     private DoctrineManager $manager;
 
+    /**
+     * CreateTaskService constructor.
+     *
+     * @param DoctrineManager $manager
+     */
     public function __construct(DoctrineManager $manager)
     {
         $this->manager = $manager;
     }
 
+    /**
+     * @param CreateTaskDto $createTaskDto
+     *
+     * @return Task
+     * @throws \Exception
+     */
     public function createTask(CreateTaskDto $createTaskDto): Task
     {
         $task = (new Task())->setTitle($createTaskDto->getTitle())
